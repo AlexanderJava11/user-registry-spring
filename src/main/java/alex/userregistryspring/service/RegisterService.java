30d785718142a84160a9b85630c89d285ce07a0b
Aleks <alexander.zeljic@yh.nackademin.se>
2026-04-17 17:07:40 +0200

    Spring Boot

--- src/main/java/alex/userregistryspring/service/RegisterService.java
+++ src/main/java/alex/userregistryspring/service/RegisterService.java
@@ -2,15 +2,19 @@ package alex.userregistryspring.service;
 
 import alex.userregistryspring.model.AppUser;
 import alex.userregistryspring.repository.AppUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
     private final AppUserRepository repository;
+    private final PasswordEncoder encoder;
 
-    public RegisterService(AppUserRepository repository) {
+    public RegisterService(AppUserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.encoder = passwordEncoder;
     }
+
 
     public boolean registerUser(String username, String password) {
         if (username == null || username.isBlank()) {
